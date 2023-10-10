0f4580ca521971207578e350b2fbbbf9e8e0003e
Sprt6I <74651719+Sprt6I@users.noreply.github.com>
2023-10-10 22:50:22 +0200

    Added class for registrating user
    Class All has functions:
    
    fname_F() for creating user first name
    lname_F() for creating user last name
    gmail_F() for creating user gmail
    
    Register(): Calls all  listed before functions, and sets users age. When all is done Ads user gmail and Phone number ? (yes phone number not created) and create user obj with User class
    
    check(var:str, minLen:int, maxLen:int, gmail:bool=True):
    checks if user: First/Last name/Gmail is valid width rules

--- Banking/Main.py
+++ Banking/Main.py
@@ -2,8 +2,10 @@ def main():
     print("\nEnter What u want: ")
     want = input("Login: 1 | Register: 2   : ")
     
+    usr = All()
+    
     if want=="2":
-        Register()
+        All.Register()
     elif want=="1":
         Login()
     else:
@@ -16,19 +18,79 @@ def main():
 
 
 """ MAIN FUNCTIONS """
-
-def Register():
-    fname = input("Enter First Name: ")
-    check(fname, 5, 15)
+class All:
+    def fname_F():
+        All.fname = input("Enter First Name: ")
+        print(All.check(All.fname, 5, 15))
+        while(All.check(All.fname, 5, 15)==-1):
+            
+            All.fname = input("Enter First Name: ")
+        
+    def lname_F():
+        All.lname = input("Enter Last Name: ")
+        while(All.check(All.lname, 8, 20)==-1):
+            All.lname = input("Enter Last Name: ")
+            
+    def gmail_F():
+        All.gmail = input("Enter Gmail: ")
+        while(All.check(All.gmail, 10, 40, gmail=True)==-1):
+            All.gmail = input("Enter Gmail: ")
     
-    lname = input("Enter Last Name: ")
-    check(lname, 8, 20)
-    
-    gmail = input("Enter Last Name: ")
-    check(lname, 8, 20)
-    
+    def Register():
+        c = 0
+        
+        All.fname_F()
+            
+        All.lname_F()
+        
+        while(True):
+            age = input("Enter Age: ")
+            if len(age)>3 or len(age)<1:
+                print("Incorrect age")
+                continue
+            elif int(age)<18 or int(age)>112:
+                print("Age between <18,112>")
+                continue
+            break
+        
+        All.gmail_F()
+        
+        print("You are now registered user !!!")
+        Database.usersGmail.append(All.gmail) #Adds gmail to database, for later check (can't be 2 accounts with same gmail (at least here)) 
+        Database.usersPhones.append(111222333) #Same here (can't be 2 accounts with same phone)
+        
+        globals()[f"{All.fname}_{All.lname}"] = User(All.fname, All.lname, age, All.gmail, 111222333)
+        print(globals()[f"{All.fname}_{All.lname}"].age)
 
+        
+        
+    def check(var:str, minLen: int, maxLen: int,gmail:bool=False) -> int:
+        """_summary_
 
+        Args:
+            var (str): variable to check
+            minLen (int): minimum length of variable
+            maxLen (int): max length of variable
+            gmail (bool, optional): if set to True it will not correct: @ . 1234567890 ' . in variable. Defaults to False.
+
+        Returns:
+            int: returns -1 if there is "error" in variable (its smaller than minimum length etc)
+        """
+    
+        if len(var)>maxLen or len(var)<minLen:
+            print(f"Name {var} be in range <{minLen},<{maxLen}>")
+            return -1
+        if gmail==False:
+            for _ in var:
+                if _ in "1234567890`!@#$%^&*()-=_+[]{}\|;:'?/.>,<~" or _ in  '"':
+                    print("No numbers or special characters allowed !")
+                    return -1
+        else:
+            for _ in var:
+                if _ in "`!#$%^&*()=+[]{}\|;:?/>,<~" or _ in  '"':
+                    print("Used invalid special characters in gmail !")
+                    return -1
+    
 
 def Login():
     pass
@@ -43,30 +105,20 @@ def Login():
 
 """ MAIN CLASSES """
 class Database:
-    usersName = []
+    usersGmail = []
+    usersPhones = []
     
 class User:
-    def __init__(self, fname, lname, age, phoneNumber):
+    def __init__(self, fname: str, lname: str, age: int, gmail: str, phoneNumber):
         self.fname = fname
         self.lname=lname
         self.age=age
+        self.gmail=gmail
         self.phoneNumber=phoneNumber
 
 
 
 
-
-
-"""HELP FUNCTIONS"""
-
-def check(var:str, minLen: int, maxLen: int):
-    if len(var)>maxLen or len(var)<minLen:
-        print("Name must be in range <5,15>")
-        Register()   
-    for _ in check:
-        if _ in "1234567890`!@#$%^&*()-=_+[]{}\|;:'?/.>,<~" or _ in  '"':
-            print("No numbers or special characters in name !")
-            Register()
             
 
 
