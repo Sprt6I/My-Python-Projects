2062ed69c55f120669e7e4a8c41ac23e2f433243
[deleted] <Sprt6I@Proton.me>
2024-01-01 16:14:52 +0100

    [UPDATE] Suprise Window
    Checks If Pip Is installed:
    * If Yes Continue
    * Else install
    
    Checks If Libraries Are Installed:
    * If Yes Continue
    * Else Installs
    
    Added More Programs:
    * Web Browsers
    * Microsoft Store
    * Steam
    * Cmd
    * Powershell

--- Projects/Suprise Window/main.py
+++ Projects/Suprise Window/main.py
@@ -1,26 +1,47 @@
-from PySide6.QtWidgets import QSizePolicy, QGridLayout, QWidget, QApplication, QLineEdit, QPushButton, QVBoxLayout, QTextEdit, QLabel
-from PySide6.QtCore import Qt
-from random import choice
 import sys
-import subprocess
 import os
-import psutil
-import signal
+import requests
 
-def terminate_process(process_name):
-   for proc in psutil.process_iter(['pid', 'name']):
-       if proc.info['name'] == process_name:
-           os.kill(proc.info['pid'], signal.SIGTERM)
+'''Checks Pip'''
+x = os.system('pip --version') #Checks if pip is installed
+  
+if x!=0: #0==installed else some other number
+  '''Install Pip'''
+  url = 'https://bootstrap.pypa.io/get-pip.py'
+  response = requests.get(url)
+  with open('get-pip.py', 'wb') as f:
+      f.write(response.content)
+      
+  os.system('py get-pip.py')
+  os.system('python get-pip.py')
+  os.system('python3 get-pip.py')
 
 
+'''Check All Librares'''
+try:
+  import psutil
+  import signal
+  from random import choice
+  from PySide6.QtWidgets import QWidget, QApplication, QPushButton, QVBoxLayout, QLabel
+except:
+  os.system('pip install psutil')
+  os.system('pip install signal')
+  os.system('pip install random')
+  os.system('pip install PySide6')
+  import psutil
+  import signal
+  from random import choice
+  from PySide6.QtWidgets import QWidget, QApplication, QPushButton, QVBoxLayout, QLabel
 
+
+'''App'''
 class App_(QWidget):
   def __init__(self):
     super().__init__()
     
     self.setWindowTitle("Hello User")
     
-    self.arr = ['a','b','c','d','e','error','f','g']
+    self.arr = ['a','b','c','d','e','error','f','g'] #Window Names
     
     self.UI_()
     
@@ -28,23 +49,30 @@ class App_(QWidget):
     self.mainLayout = QVBoxLayout()
     
     self.button = QPushButton("hehe")
-    self.button.clicked.connect(self.Yep_)
+    self.button.clicked.connect(self.ChangeWindowName)
     self.mainLayout.addWidget(self.button)
     
     self.label = QLabel("error")
-    #mainLayout.addWidget(self.label)
     
     self.setLayout(self.mainLayout)
     
     
-  def Yep_(self):
+  def ChangeWindowName(self):
     val = choice(self.arr)
     self.setWindowTitle(val)
     
     if val=="error":
       self.button.deleteLater()
       self.mainLayout.addWidget(self.label)
-      terminate_process('brave')
+      self.TerminateProcess_()
+      
+  
+  def TerminateProcess_(self):
+   arr = ['steam', 'brave', 'opera', 'chrome', 'edge', 'microsoft-store', 'firefox', 'powershell', 'cmd', 'safari']
+   for proc in psutil.process_iter(['pid', 'name']):
+      print(proc.pid, proc.name)
+      if proc.info['name'] in arr:
+        os.kill(proc.info['pid'], signal.SIGTERM)
       
       
 if __name__ == "__main__":
