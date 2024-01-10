3a59726edb1529aeaf104b3a06d2d3465f3848fa
[deleted] <Sprt6I@Proton.me>
2024-01-10 13:45:33 +0100

    [Update + Fixes]
    Fixed:
    * Buttons now change localization as they should
    
    Added:
    * Direction Bar
    * Scroll For Directories

--- Projects/File Explorer/main.py
+++ Projects/File Explorer/main.py
@@ -1,6 +1,8 @@
-from PySide6.QtWidgets import QSizePolicy,QMainWindow, QGridLayout, QWidget, QApplication, QLineEdit, QPushButton, QVBoxLayout, QTextEdit
+from PySide6.QtWidgets import QSizePolicy,QMainWindow, QGridLayout, QWidget, QApplication, QLineEdit, QPushButton, QVBoxLayout, QTextEdit, QScrollArea
+from PySide6.QtCore import Qt
 import sys
 import os
+from functools import partial
 
 class FolderManager__(QMainWindow):
   def __init__(self):
@@ -12,14 +14,32 @@ class FolderManager__(QMainWindow):
   def UI_(self):
     mainWidget = QWidget()
     self.mainLayout = QVBoxLayout()
+    self.itemsWidget = QWidget()
+    self.itemsLayout = QVBoxLayout()
+    self.scroll = QScrollArea() #type: ignore
     
-    self.mainLayout.setSpacing(0)
+    
+    self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn) #type: ignore
+    self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff) #type: ignore
+    self.scroll.setWidgetResizable(True)
+    
+    
+    self.Directories = QLineEdit(os.getcwd())
+    self.Directories.returnPressed.connect(lambda: self.ChangedDirectory_(self.Directories.text()))
+    self.mainLayout.addWidget(self.Directories)
+    
     
     self.CreateButtons_()
-      
-    mainWidget.setLayout(self.mainLayout)
+    self.itemsWidget.setLayout(self.itemsLayout)
+    self.scroll.setWidget(self.itemsWidget)
     
+    
+    self.mainLayout.addWidget(self.scroll)
+    
+    
+    mainWidget.setLayout(self.mainLayout)
     self.setCentralWidget(mainWidget)
+    
     
     
   def CreateButtons_(self):  
@@ -27,27 +47,33 @@ class FolderManager__(QMainWindow):
     print(arr)
     
     for i in arr:
-      but = QPushButton(f"{i}")
+      but = QPushButton(i, self)
       #but.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding) #type:ignore
-      but.clicked.connect(lambda: self.ChangeLoc_(i))
-      self.mainLayout.addWidget(but)
+      but.clicked.connect(partial(self.ChangeLoc_, i))
+      self.itemsLayout.addWidget(but)
     
-    self.update()
+    #self.update()
     #return os.listdir(os.getcwd())
   
   
   def ChangeLoc_(self, where: str):
-    for i in range(self.mainLayout.count()):
-        child = self.mainLayout.itemAt(i).widget()
+    print(where)
+    for i in range(self.itemsLayout.count()):
+        child = self.itemsLayout.itemAt(i).widget()
         if child:
             child.deleteLater()
             
     os.chdir(where)
             
-    self.mainLayout.setSpacing(0)
-    self.adjustSize()
+    self.itemsLayout.setSpacing(0)
+    #self.adjustSize()
             
     self.CreateButtons_()
+    self.Directories.setText(os.getcwd())
+    
+  def ChangedDirectory_(self, text):
+    self.ChangeLoc_(text)
+    #print(f"LineEdit Pressed! {text}")
   
   
   
