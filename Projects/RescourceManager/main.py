bfe557c248548f6aac1fc704d447c7b10c7d0b9f
Sprt6I <74651719+Sprt6I@users.noreply.github.com>
2024-01-13 23:40:44 +0100

    Update main.py

--- Projects/RescourceManager/main.py
+++ Projects/RescourceManager/main.py
@@ -1,5 +1,9 @@
-from PySide6.QtWidgets import QWidget, QApplication, QPushButton, QVBoxLayout, QLabel, QMainWindow 
+from PySide6.QtWidgets import QWidget, QApplication, QPushButton, QVBoxLayout, QLabel, QMainWindow, QHBoxLayout
+from PySide6.QtCore import Signal
 import psutil
+import GPUtil
+import time
+import threading
 
 print('\nPROCESSOR')
 print(psutil.cpu_count())
@@ -7,7 +11,7 @@ print(psutil.cpu_freq())
 print(psutil.cpu_percent())
 
 print('\nHARD DRIVE')
-print(psutil.disk_usage('/').percent)
+print()
 
 print('\nRAM')
 print(psutil.virtual_memory().total)
@@ -15,10 +19,17 @@ print(psutil.virtual_memory().available)
 print(psutil.virtual_memory().used)
 print(psutil.virtual_memory().percent)
 
-print('\nFANS')
+
+print('\nGpu')
+gpus = GPUtil.getGPUs()
+
+for gpu in gpus:
+   print(gpu.name)
+
+'''print('\nFANS')
 arr = psutil.sensors_fans()
 for fan in psutil.sensors_fans():
-  print(fan, arr[fan][0].current) #Name, usage
+  print(fan, arr[fan][0].current) #Name, usage'''
   
   
 class RescourceMonitor__(QMainWindow):
@@ -31,31 +42,91 @@ class RescourceMonitor__(QMainWindow):
     
   def UI_(self):
     mainWidget = QWidget()
-    mainLayout = QVBoxLayout()
+    self.mainLayout = QHBoxLayout()
     
+    self.SetCpu_()
+    
+    self.SetRam_()
+    
+    self.SetDisk()
+    
+    mainWidget.setLayout(self.mainLayout)
+    
+    self.setCentralWidget(mainWidget)
+    
+    
+  def SetCpu_(self):
     cpuLayout = QVBoxLayout()
-    
-    
     cputLabel = QLabel("CPU:")
     cpuLayout.addWidget(cputLabel)
     
     cpuCountLabel = QLabel(f'{psutil.cpu_count()}')
     cpuLayout.addWidget(cpuCountLabel)
     
-    cpuFrequencyMax = QLabel(f'{psutil.cpu_freq().max}')
-    cpuLayout.addWidget(cpuFrequencyMax)
+    self.cpuProcent = QLabel(f'{psutil.cpu_percent()}')
+    cpuLayout.addWidget(self.cpuProcent)
     
-    cpuFrequencyMin = QLabel(f'{psutil.cpu_freq().min}')
-    cpuLayout.addWidget(cpuFrequencyMin)
+    self.cpuFrequencyMax = QLabel(f'{psutil.cpu_freq().max}')
+    cpuLayout.addWidget(self.cpuFrequencyMax)
+    
+    self.cpuFrequencyMin = QLabel(f'{psutil.cpu_freq().min}')
+    cpuLayout.addWidget(self.cpuFrequencyMin)
+    
+    threading.Thread(target=self.UpDateCpu_, daemon=True).start()
+    
+    self.mainLayout.addLayout(cpuLayout)
+    
+  
+  def UpDateCpu_(self):
+    while 1:
+      self.cpuProcent.setText(f'{psutil.cpu_percent()}%')
+      self.cpuFrequencyMax.setText(f'{psutil.cpu_freq().max}')
+      self.cpuFrequencyMin.setText(f'{psutil.cpu_freq().min}')
+      time.sleep(0.3) 
+  
+  def SetRam_(self):
+    ramLayout = QVBoxLayout()
+    
+    ramLabel = QLabel('RAM:')
+    ramLayout.addWidget(ramLabel)
+    
+    ramTotal = QLabel(f'{psutil.virtual_memory().total // 1024 //1024//1024} GB')
+    ramLayout.addWidget(ramTotal)
+    
+    self.ramPrecent = QLabel(f'{psutil.virtual_memory().percent}%')
+    ramLayout.addWidget(self.ramPrecent)
+    
+    self.ramAviable = QLabel(f'{psutil.virtual_memory().available //1024 //1024//1024} GB')
+    ramLayout.addWidget(self.ramAviable)
+    
+    self.ramUsed = QLabel(f'{psutil.virtual_memory().used //1024 //1024//1024} GB')
+    ramLayout.addWidget(self.ramUsed)
+    
+    threading.Thread(target=self.UpdateRam_, daemon=True).start()
+    
+    self.mainLayout.addLayout(ramLayout)
+    
+  def UpdateRam_(self):
+    while 1:
+      self.ramPrecent.setText(f'{psutil.virtual_memory().percent}%')
+      self.ramAviable.setText(f'{psutil.virtual_memory().available //1024 //1024//1024} GB')
+      self.ramUsed.setText(f'{psutil.virtual_memory().used //1024 //1024//1024} GB')
+      time.sleep(0.3)
+      
+  def SetDisk(self):
+    diskLayout = QVBoxLayout()
+    
+    diskLabel = QLabel("DISK:")
+    diskLayout.addWidget(diskLabel)
     
     
-    mainLayout.addLayout(cpuLayout)
+    
+    for disk in psutil.disk_partitions():
+      diskProcentage = QLabel(f'{disk.device[:-1]}' + ' | ' + f'{psutil.disk_usage(disk.device).free //1024//1024//1024} GB' + ' | ' + f'{psutil.disk_usage(disk.device).percent}%')
+      diskLayout.addWidget(diskProcentage)
     
     
-    mainWidget.setLayout(mainLayout)
-    
-    self.setCentralWidget(mainWidget)
-    
+    self.mainLayout.addLayout(diskLayout)
     
     
     
